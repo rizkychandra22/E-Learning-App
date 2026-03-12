d423890434900c5654204eeac99ddaf0279ea2d5
gilangmubharak <gilangmubharak@gmail.com>
2026-03-12 18:34:42 +0700

    Seed demo finance data for top piutang

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             FakultasSeeder::class,
             JurusanSeeder::class,
+            FinanceDemoSeeder::class,
         ]);
     }
 }
