7cecf78771b502841d940e77897758ba016a93a7
gilangmubharak <gilangmubharak@gmail.com>
2026-03-12 18:35:07 +0700

    Enforce consistent KPI card height

--- resources/js/lib/card.js
+++ resources/js/lib/card.js
@@ -1,3 +1,2 @@
-export const KPI_CARD_HEIGHT_CLASS = 'h-[120px]';
-export const KPI_CARD_BASE_CLASS = 'rounded-xl border border-border bg-card shadow-card p-3 flex flex-col justify-between';
-
+export const KPI_CARD_HEIGHT_CLASS = 'h-[110px]';
+export const KPI_CARD_BASE_CLASS = 'h-[110px] rounded-xl border border-border bg-card shadow-card p-3 flex flex-col justify-between';
