431572cc41aa1efa471503bcffe32a71e36a542d
Gilang F - Mubharak <61201149+gilangmubharak@users.noreply.github.com>
2026-04-10 19:26:56 +0700

    chore(frontend): adjust app css import order

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,7 +1,7 @@
 import './bootstrap';
-import '../css/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../css/app.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import { createRoot } from 'react-dom/client';
